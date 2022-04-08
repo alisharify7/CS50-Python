80852945be59403f9f9a8f72ad0cf57a38f2366e
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-08 03:11:53 -0600

    Add files via upload

---
+++ Lecture 0,1,2/tip/tip.py
@@ -0,0 +1,25 @@
+
+
+def main():
+    dollars_to_float(input("How much was the meal? "))
+    #percent = percent_to_float(input("What percentage would you like to tip? "))
+    #tip = dollars * percent
+    #print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # TODO
+    for each in range(1,len(d)):
+        print(d[each])
+
+
+
+def percent_to_float(p):
+    # TODO
+
+
+
+
+main()
+
+
