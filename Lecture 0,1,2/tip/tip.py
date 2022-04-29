c4531e89d6206108800ba03e444f21299f399c70
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-04-29 20:17:25 +0430

    Update tip.py

--- Lecture 0,1,2/tip/tip.py
+++ Lecture 0,1,2/tip/tip.py
@@ -21,3 +21,28 @@ def percent_to_float(p):
 
 
 main()
+
+
+# another way with string sliceing method
+#def main():
+#    dollars = dollars_to_float(input("How much was the meal? "))
+#    percent = percent_to_float(input("What percentage would you like to tip? "))
+#    tip = dollars * percent
+#    print(f"Leave ${tip:.2f}")
+
+
+#def dollars_to_float(d):
+#    d = d[1:]
+#    d = float(d)
+#    return d
+
+
+#def percent_to_float(p):
+#    temp = p
+#    location = temp.find("%")
+#    temp = temp[:location]
+#   temp = int(temp)
+#    return (temp / 100)
+
+
+#main()
