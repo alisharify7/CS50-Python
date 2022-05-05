96dece616f11c2e31ddca934444276d57ac77def
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-05 19:39:59 +0430

    Update bank.py

--- week-1/bank/bank.py
+++ week-1/bank/bank.py
@@ -1,11 +1,10 @@
 # get input from user and strip it and force it to be UPPER case
 user = input("Greeting: ").strip().upper()
 
-
 if ("HELLO" in user):
     print("$0")
+
 # if user input start with H so print 20 $ 
-   
 elif(user[0] == 'H'):
     print("$20")
 
