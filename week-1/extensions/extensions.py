03ce9e5ab721d05db4b86fe00d98ea0b9d036a40
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-05 20:03:38 +0430

    Update extensions.py

--- week-1/extensions/extensions.py
+++ week-1/extensions/extensions.py
@@ -1,38 +1,14 @@
-# import sys
-
-# user = input("File name: ").lower()
-
-# image = ['.gif','.jpeg','.png']
-# app = ['.pdf','.zip']
-
-# for i in image:
-#     if (i in user):
-#         a = i.replace(".","")
-#         print("image/" + a)
-#         sys.exit(0)
-
-# for i in app:
-#     if (i in user):
-#         a = i.replace(".","")
-#         print("application/" + a)
-#         sys.exit(0)
-
-# if ('.txt' in user):
-#     print("text/plain")
-# elif('.jpg' in user):
-#     print("image/jpeg")
-# else:
-#     print("application/octet-stream")
-
-
 import sys
 
+# Get input from user and strip it and force it to be lower case
 user = input("File name: ").strip().lower()
+
 
 image = ['.gif','.png']
 app = ['.pdf','.zip']
 jpeg = ['.jpg','jpeg']
 
+# iterrate in each list and find file extensions
 
 for i in image:
     if (i in user):
