75db990fc2f0bff89da8480d6826b366c3ca87e5
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-05 10:01:09 -0600

    Add files via upload

---
+++ week-1/meal/meal.py
@@ -0,0 +1,11 @@
+user = input("What time is it? ").strip()
+
+hour,time =user.split(":")
+hour = int(hour)
+
+if(7 <= hour <= 8):
+    print("Breakfast time")
+elif(12 <= hour <= 13):
+    print("lunch time")
+elif(18 <= hour <= 19):
+    print("dinner time")
