b5b8cb5d47ff4147c9a0661bc382e25e4e326e91
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-05 19:38:40 +0430

    Delete week1 directory

--- week1/deep/deep.py
+++
@@ -1,8 +0,0 @@
-# get input from user and strip it and force it to be UPPER case
-user = input("What is the Answer to the Great Question of Life, the universe, and Everything? ").strip().upper()
-
-#condition for check user input
-if (user in ['42','FORTY-TWO', 'FORTY TWO']):
-    print("Yes")
-else:
-    print("No")    
