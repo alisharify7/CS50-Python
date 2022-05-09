414fe634ce0a2f7dbcddc2b80d2675cd9c8c3845
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-09 21:58:07 +0430

    Update tip.py

--- week-0/tip/tip.py
+++ week-0/tip/tip.py
@@ -1,3 +1,13 @@
+############################################
+#                                          #
+#             by : Ali Sharify             #
+#        Dont copy currently Code          #
+#          Github: alisharifyy             #
+#                                          #
+############################################
+
+
+
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
