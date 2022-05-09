fe24bad16b56140bc17aa2306533ba6ed8e3e52e
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-09 21:58:53 +0430

    Update deep.py

--- week-1/deep/deep.py
+++ week-1/deep/deep.py
@@ -1,3 +1,13 @@
+############################################
+#                                          #
+#             by : Ali Sharify             #
+#        Dont copy currently Code          #
+#          Github: alisharifyy             #
+#                                          #
+############################################
+
+
+
 # get input from user and strip it and force it to be UPPER case
 user = input("What is the Answer to the Great Question of Life, the universe, and Everything? ").strip().upper()
 
