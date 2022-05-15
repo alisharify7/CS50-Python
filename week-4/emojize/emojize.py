5666b8862faf6f356f634e1bb72b0539f6ed8083
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-05-15 19:45:53 +0430

    Update emojize.py

--- week-4/emojize/emojize.py
+++ week-4/emojize/emojize.py
@@ -1,3 +1,16 @@
+
+############################################
+#                                          #
+#             by : Ali Sharify             #
+#        Dont copy currently Code          #
+#          Github: alisharifyy             #
+#                                          #
+############################################
+
+
+
 import emoji as Em  
+
 input_user = input("input: ")
+
 print(Em.emojize(f"output: {input_user}"))
