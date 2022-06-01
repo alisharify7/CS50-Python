52fc655a09b1bec92f76c8b4f6e4e8297ceb077c
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-06-01 14:27:49 +0430

    add file

---
+++ week-4/bitcoin/bitcoin.py
@@ -0,0 +1,29 @@
+import requests
+import sys
+
+
+if len(sys.argv) == 2:
+
+    # covert command line argument to float Number
+    try:    
+        user_input = float(sys.argv[1])
+    except ValueError:
+        print("Command-line argument is not number")
+    # if yser input in not Number
+        sys.exit(1)
+else:
+    # if user not cooperate and Enter a Number
+    print("Missing command-line argument")
+    sys.exit(1)
+
+try:
+    # calling api
+    answer = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    price = float(answer['bpi']['USD']['rate_float'])
+    price = price * user_input
+    print(f"${price:,.4f}")
+    sys.exit(0)
+    
+except (requests.RequestException,ValueError):
+    print("Error")
+    sys.exit(1)
