08476916e77be4bf16095250754cf6bc5576cf3d
ali sharifi <87280202+alisharifyy@users.noreply.github.com>
2022-10-07 19:25:01 +0330

    add regex to app

--- week-0/playback/playback.py
+++ week-0/playback/playback.py
@@ -7,7 +7,6 @@
 ############################################
 
 
-import sys
 
 # get input from user and strip all white space
 s = input().strip()
@@ -21,3 +20,22 @@ for i in range(len(s)):
 
 # new line in end program
 print("")
+
+
+
+
+# better version of this program using regex
+"""
+import re 
+
+
+x = input("")
+def better_version(input):
+    if not input:
+        return "Empty"
+
+    result = re.sub(" ","...",input)
+    return result
+
+print(better_version(x))
+"""
