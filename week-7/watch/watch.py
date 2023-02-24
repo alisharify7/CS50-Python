979be0dec570155890447361455e0889708f608a
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2023-02-24 05:24:15 -0500

    add watch

---
+++ week-7/watch/watch.py
@@ -0,0 +1,27 @@
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    """
+        this function take an youtube iframe like and return short version of link
+    """
+
+    #  - user walrus -  
+    # if src_link := re.search(r"^<iframe(?:.+)? src=\"(?:https?://)?(?:www\.)?youtube.com/embed/(\w+)\".?(?:.+)?></iframe>$", s):
+    #     return f"https://youtu.be/{src_link.group(1)}"
+    # return None
+
+    
+    #  - normal way -
+    src_link = re.search(r"^<iframe(?:.+)? src=\"(?:https?://)?(?:www\.)?youtube.com/embed/(\w+)\".?(?:.+)?></iframe>$", s):
+    if src_link:
+         return f"https://youtu.be/{src_link.group(1)}"
+    return None
+
+
+if __name__ == "__main__":
+    main()
